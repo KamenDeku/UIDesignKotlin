c3090b086c9d9bfa0e954cf6a97cb38b61a2ee76
RaulMun <raulmuniz38@gmail.com>
2025-10-05 23:22:34 -0600

    Add back button functionality in PlayActivity and fixed activityplay crashing issue

--- app/src/main/java/com/example/uidesignkotlin/PlayActivity.kt
+++ app/src/main/java/com/example/uidesignkotlin/PlayActivity.kt
@@ -2,10 +2,19 @@ package com.example.uidesignkotlin
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.widget.ImageButton
+import android.content.Intent
 
 class PlayActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
+
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
